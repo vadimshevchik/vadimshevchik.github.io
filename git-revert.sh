a7d3d3be2995ff0095ed21295c43966201e8ab2c
Vadim Shevchik <vadim.shevchik@silkailines.com>
2023-07-13 12:30:05 +0300

    Updated git clean script

--- git-revert.sh
+++ git-revert.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+# Deletes files/dirs listed in .gitignore (like build files)
+# Deletes files/dirs that are not tracked and not in .gitignore
+env -i git clean --force -d -x
+# Reverts changes maded in tracked files,
+# Restores deleted tracked files
 env -i git reset --hard
