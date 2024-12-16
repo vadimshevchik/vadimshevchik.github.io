88771f0222a0b3f2349e8a841a0426f917556190
Vadim Shevchik <vadim.shevchik@silkailines.com>
2024-12-16 13:22:05 +0300

    multi dl pages

--- default.js
+++ default.js
@@ -4,7 +4,7 @@ function main() {
   const bundleId = params.get("bundleId");
   const link = params.get("link");
   const platform = getPlatgorm();
-  if (platform == "iOS" || platform == "Mac") {
+  if (platform == "iOS") {
     const url = "https://apps.apple.com/de/app/id" + appId;
     window.location.replace(url);
   } else if (platform == "Android") {
@@ -16,11 +16,13 @@ function main() {
 }
 
 function getPlatgorm() {
-  var uA = navigator.userAgent || navigator.vendor || window.opera;
-  if ((/iPad|iPhone|iPod/.test(uA) && !window.MSStream) || (uA.includes('Mac') && 'ontouchend' in document)) return 'iOS';
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+  if ((/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) || (userAgent.includes('Mac') && 'ontouchend' in document)) {
+    return 'iOS';
+  }
 
-  var i, os = ['Windows', 'Android', 'Linux', 'Mac', 'iOS'];
-  for (i = 0; i < os.length; i++) if (new RegExp(os[i],'i').test(uA)) return os[i];
+  var i, os = ['Android', 'iOS']; // 'Mac', 'Linux', 'Windows'
+  for (i = 0; i < os.length; i++) if (new RegExp(os[i],'i').test(userAgent)) return os[i];
 }
 
 main();
